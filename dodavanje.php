e6a5c8facd0e09c6df6b450f8d137c432285fb76
Zamachi <dstefa.dimitrijevic7@gmail.com>
2022-01-26 00:27:44 +0100

    Dodato azuriranjeNeo, sredjivanje merenja za dodavanja

--- dodavanje.php
+++ dodavanje.php
@@ -1,38 +1,22 @@
 <?php
 
-$konekcija = new mysqli('127.0.0.1','root','', 'nosql');
+$konekcija = new mysqli('127.0.0.1', 'root', '', 'nosql');
 
 // Normalizacija ulaznih parametara
 $br_entiteta = (int) $_REQUEST['br_entiteta'];
 $br_atributa = (int) $_REQUEST['br_atributa'];
-
-$merenja_dodavanja_entiteta=[];
-$merenja_dodavanja_atributa=[];
-$merenje_jedne_iteracije=[];
 
 $konekcija->query('TRUNCATE TABLE Attribute');
 $konekcija->query('TRUNCATE TABLE Entity');
 
 $pocetak_merenja = microtime(true);
 // Dodavanje entiteta i atributa u bazu
-for ($i=0; $i<$br_entiteta; $i++)
-{
-    $pocetak_merenja_jedne_iteracije = microtime(true); //merimo celu iteraciju ($i)
-    
-    $pocetak_merenja_dodavanja_entiteta = microtime(true); //merimo dodavanje jednog reda u tabeli Entity
+for ($i = 0; $i < $br_entiteta; $i++) {
     $result = $konekcija->query('insert into Entity values ()'); //insertuje Entitet(ima samo polje primarnog kljuca koje se automatski inkrem.)
-    array_push($merenja_dodavanja_entiteta, ( microtime(true) - $pocetak_merenja_dodavanja_entiteta ));
-    
-    $entity_id = $konekcija->insert_id;//dohvatamo ID generisan AUTO_INCREMENT-om iz prethodnog query-ja ^
-    
-    $pocetak_merenja_dodavanja_atributa = microtime(true);//merimo dodavanje jednog reda u tabeli Attribute
-    for ($j=0; $j<$br_atributa; $j++)
-    {
+    $entity_id = $konekcija->insert_id; //dohvatamo ID generisan AUTO_INCREMENT-om iz prethodnog query-ja ^
+    for ($j = 0; $j < $br_atributa; $j++) {
         $resu1t = $konekcija->query("insert into Attribute (Entity_ID, Name, Value) values ($entity_id, 'A{$j}', 'V{$j}')");
     }
-    array_push($merenja_dodavanja_atributa, ( microtime(true) - $pocetak_merenja_dodavanja_atributa ));
-    
-    array_push($merenje_jedne_iteracije, ( microtime(true) - $pocetak_merenja_jedne_iteracije ));
 }
 $kraj_svih_upisa = microtime(true) - $pocetak_merenja;
 
@@ -46,55 +30,22 @@ $konekcija->close();
 ?>
 
 <html>
-    <body>
-        <hr>
-        <h1>
-            Engine koriscen je: <?php echo $engine; ?> <br>
-            Broj entiteta je: <?php echo $br_entiteta; ?> <br>
-            Broj atributa je: <?php echo $br_atributa; ?> <br>
-        </h1>
-        <hr><br><hr>
-        <div>
-            <h2>
-                Dodavanje entiteta
-            </h2><br>
-            <p>
-                Dodavanje jednog cvora u proseku traje: <?php echo array_sum($merenja_dodavanja_entiteta)/count($merenja_dodavanja_entiteta); ?>&nbsp; sekundi. 
-            </p>
-            <p>
-                Dodavanje svih cvorova trajalo je: <?php echo array_sum($merenja_dodavanja_entiteta); ?>&nbsp; sekundi. 
-            </p>
-        </div>
-        <hr><br><hr>
-        <div>
-            <h2>
-                Dodavanje Atributa
-            </h2><br>
-            <p>
-                Dodavanje jednog cvora u proseku traje: <?php echo array_sum($merenja_dodavanja_atributa)/count($merenja_dodavanja_atributa); ?>&nbsp; sekundi. 
-            </p>
-            <p>
-                Dodavanje svih cvorova trajalo je: <?php echo array_sum($merenja_dodavanja_atributa); ?>&nbsp; sekundi. 
-            </p>
-        </div>
-        <hr><br><hr>
-        <div>
-            <h2>
-                Jedna iteracija
-            </h2><br>
-            <p>
-                Jedna iteracija u proseku traje: <?php echo array_sum($merenje_jedne_iteracije)/count($merenje_jedne_iteracije); ?>&nbsp; sekundi. 
-            </p>
-            <p>
-                Cela for petlja: <?php echo array_sum($merenje_jedne_iteracije); ?>&nbsp; sekundi. 
-            </p>
-        </div>
-        <hr><br><hr>...
-        <div>
-            <p>
-                Sveukupno je trajalo: <?php echo $kraj_svih_upisa;?>&nbsp; sekundi.
-            </p>
-        </div>
-        <hr>
-    </body>
+
+<body>
+    <hr>
+    <h1>
+        Engine koriscen je: <?php echo $engine; ?> <br>
+        Broj entiteta je: <?php echo $br_entiteta; ?> <br>
+        Broj atributa je: <?php echo $br_atributa; ?> <br>
+    </h1>
+    <hr><br>
+    <hr>
+    <div>
+        <p>
+            Sveukupno je trajalo: <?php echo $kraj_svih_upisa; ?>&nbsp; sekundi.
+        </p>
+    </div>
+    <hr>
+</body>
+
 </html>
--- dodavanjeMongo.php
+++ dodavanjeMongo.php
@@ -5,20 +5,9 @@ require_once "./mongoDBManager.php";
 $br_entiteta = (int) $_REQUEST['br_entiteta_mongo'];
 $br_atributa = (int) $_REQUEST['br_atributa_mongo'];
 
-$merenja_dodavanja_entiteta=[];
-$merenja_dodavanja_atributa=[];
-$merenje_jedne_iteracije=[];
-
 $pocetak_merenja = microtime(true);
 for ($i=0; $i < $br_entiteta; $i++) { 
-    $pocetak_merenja_jedne_iteracije = microtime(true); //merimo celu iteraciju ($i)
-    
-    $pocetak_merenja_dodavanja_entiteta = microtime(true); //merimo dodavanje jednog reda u tabeli Entity
     $entityInsertResult = $entities->insertone([]);
-    
-    array_push($merenja_dodavanja_entiteta, ( microtime(true) - $pocetak_merenja_dodavanja_entiteta ));
-    
-    $pocetak_merenja_dodavanja_atributa = microtime(true);//merimo dodavanje jednog reda u tabeli Attribute
     for ($j=0; $j < $br_atributa ; $j++) { 
         $attributes->insertone([
             "Name" => "A{$j}",
@@ -26,10 +15,6 @@ for ($i=0; $i < $br_entiteta; $i++) {
             "EntityID" => $entityInsertResult->getInsertedId()
         ]);
     }
-
-    array_push($merenja_dodavanja_atributa, ( microtime(true) - $pocetak_merenja_dodavanja_atributa ));
-    
-    array_push($merenje_jedne_iteracije, ( microtime(true) - $pocetak_merenja_jedne_iteracije ));
 }
 $kraj_svih_upisa = microtime(true) - $pocetak_merenja;
 ?>
@@ -42,43 +27,6 @@ $kraj_svih_upisa = microtime(true) - $pocetak_merenja;
             Broj entiteta je: <?php echo $br_entiteta; ?> <br>
             Broj atributa je: <?php echo $br_atributa; ?> <br>
         </h1>
-        <hr><br><hr>
-        <div>
-            <h2>
-                Dodavanje entiteta
-            </h2><br>
-            <p>
-                Dodavanje jednog cvora u proseku traje: <?php echo array_sum($merenja_dodavanja_entiteta)/count($merenja_dodavanja_entiteta); ?>&nbsp; sekundi. 
-            </p>
-            <p>
-                Dodavanje svih cvorova trajalo je: <?php echo array_sum($merenja_dodavanja_entiteta); ?>&nbsp; sekundi. 
-            </p>
-        </div>
-        <hr><br><hr>
-        <div>
-            <h2>
-                Dodavanje Atributa
-            </h2><br>
-            <p>
-                Dodavanje jednog cvora u proseku traje: <?php echo array_sum($merenja_dodavanja_atributa)/count($merenja_dodavanja_atributa); ?>&nbsp; sekundi. 
-            </p>
-            <p>
-                Dodavanje svih cvorova trajalo je: <?php echo array_sum($merenja_dodavanja_atributa); ?>&nbsp; sekundi. 
-            </p>
-        </div>
-        <hr><br><hr>
-        <div>
-            <h2>
-                Jedna iteracija
-            </h2><br>
-            <p>
-                Jedna iteracija u proseku traje: <?php echo array_sum($merenje_jedne_iteracije)/count($merenje_jedne_iteracije); ?>&nbsp; sekundi. 
-            </p>
-            <p>
-                Cela for petlja: <?php echo array_sum($merenje_jedne_iteracije); ?>&nbsp; sekundi. 
-            </p>
-        </div>
-        <hr><br><hr>...
         <div>
             <p>
                 Sveukupno je trajalo: <?php echo $kraj_svih_upisa;?>&nbsp; sekundi.
